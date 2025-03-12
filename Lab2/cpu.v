a7025e0f2407a70259c46e529721a0400883864e
jae1u <yjw_0412@naver.com>
2025-03-12 17:53:04 +0000

    feat: Complete cpu and control unit implementation

--- Lab2/cpu.v
+++ Lab2/cpu.v
@@ -13,37 +13,63 @@ module cpu(input reset,                     // positive reset signal
            output is_halted,                // Whehther to finish simulation
            output [31:0] print_reg [0:31]); // TO PRINT REGISTER VALUES IN TESTBENCH (YOU SHOULD NOT USE THIS)
   /***** Wire declarations *****/
+  wire [31:0] next_pc;
+  wire [31:0] instruction;
+  wire [31:0] rs1_data;
+  wire [31:0] rs2_data;
+  wire [31:0] mem_data;
+  wire alu_bcond;
+  wire [31:0] target;
+  wire PCSrc1;
+
+  wire is_jal;
+  wire is_jalr;
+  wire branch;
+  wire mem_read;
+  wire mem_to_reg;
+  wire mem_write;
+  wire alu_src;
+  wire write_enable;
+  wire pc_to_reg;
+  wire is_ecall;
 
   /***** Register declarations *****/
+  reg [31:0] current_pc;
+  reg [3:0] alu_op;
+  reg [31:0] imm_gen_out;
+  reg [31:0] alu_result;
 
   // ---------- Update program counter ----------
   // PC must be updated on the rising edge (positive edge) of the clock.
+  assign target = current_pc + imm_gen_out;
+  assign PCSrc1 = is_jal || (branch && alu_bcond);
+  assign next_pc = is_jalr ? alu_result : (PCSrc1 ? target : (current_pc + 4));
   pc pc(
-    .reset(),       // input (Use reset to initialize PC. Initial value must be 0)
-    .clk(),         // input
-    .next_pc(),     // input
-    .current_pc()   // output
+    .reset(reset),       // input (Use reset to initialize PC. Initial value must be 0)
+    .clk(clk),         // input
+    .next_pc(next_pc),     // input
+    .current_pc(current_pc)   // output
   );
   
   // ---------- Instruction Memory ----------
   instruction_memory imem(
-    .reset(),   // input
-    .clk(),     // input
-    .addr(),    // input
-    .dout()     // output
+    .reset(reset),   // input
+    .clk(clk),     // input
+    .addr(current_pc),    // input
+    .dout(instruction)     // output
   );
 
   // ---------- Register File ----------
   register_file reg_file (
-    .reset (),        // input
-    .clk (),          // input
-    .rs1 (),          // input
-    .rs2 (),          // input
-    .rd (),           // input
-    .rd_din (),       // input
-    .write_enable (), // input
-    .rs1_dout (),     // output
-    .rs2_dout (),     // output
+    .reset (reset),        // input
+    .clk (clk),          // input
+    .rs1 (instruction[19:15]),          // input
+    .rs2 (instruction[24:20]),          // input
+    .rd (instruction[11:7]),           // input
+    .rd_din (pc_to_reg ? next_pc : (mem_to_reg ? mem_data : alu_result)),       // input
+    .write_enable (write_enable), // input
+    .rs1_dout (rs1_data),     // output
+    .rs2_dout (rs2_data),     // output
     .print_reg (print_reg)  //DO NOT TOUCH THIS
   );
 
@@ -51,47 +77,47 @@ module cpu(input reset,                     // positive reset signal
   // ---------- Control Unit ----------
   control_unit ctrl_unit (
     .part_of_inst(),  // input
-    .is_jal(),        // output
-    .is_jalr(),       // output
-    .branch(),        // output
-    .mem_read(),      // output
-    .mem_to_reg(),    // output
-    .mem_write(),     // output
-    .alu_src(),       // output
-    .write_enable(),  // output
-    .pc_to_reg(),     // output
-    .is_ecall()       // output (ecall inst)
+    .is_jal(is_jal),        // output
+    .is_jalr(is_jalr),       // output
+    .branch(branch),        // output
+    .mem_read(mem_read),      // output
+    .mem_to_reg(mem_to_reg),    // output
+    .mem_write(mem_write),     // output
+    .alu_src(alu_src),       // output
+    .write_enable(write_enable),  // output
+    .pc_to_reg(pc_to_reg),     // output
+    .is_ecall(is_ecall)       // output (ecall inst)
   );
 
   // ---------- Immediate Generator ----------
   immediate_generator imm_gen(
-    .part_of_inst(),  // input
-    .imm_gen_out()    // output
+    .part_of_inst(instruction),  // input
+    .imm_gen_out(imm_gen_out)    // output
   );
 
   // ---------- ALU Control Unit ----------
   alu_control_unit alu_ctrl_unit (
-    .part_of_inst(),  // input
-    .alu_op()         // output
+    .part_of_inst({instruction[31:25], instruction[14:12], instruction[6:0]}),  // input
+    .alu_op(alu_op)         // output
   );
 
   // ---------- ALU ----------
   alu alu (
-    .alu_op(),      // input
-    .alu_in_1(),    // input  
-    .alu_in_2(),    // input
-    .alu_result(),  // output
-    .alu_bcond()    // output
+    .alu_op(alu_op),      // input
+    .alu_in_1(rs1_data),    // input  
+    .alu_in_2(alu_src ? imm_gen_out : rs2_data),    // input
+    .alu_result(alu_result),  // output
+    .alu_bcond(alu_bcond)    // output
   );
 
   // ---------- Data Memory ----------
   data_memory dmem(
-    .reset (),      // input
-    .clk (),        // input
-    .addr (),       // input
-    .din (),        // input
-    .mem_read (),   // input
-    .mem_write (),  // input
-    .dout ()        // output
+    .reset (reset),      // input
+    .clk (clk),        // input
+    .addr (alu_result),       // input
+    .din (rs2_data),        // input
+    .mem_read (mem_read),   // input
+    .mem_write (mem_write),  // input
+    .dout (mem_data)        // output
   );
 endmodule
