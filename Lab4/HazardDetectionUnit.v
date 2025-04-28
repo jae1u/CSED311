7494ba0c67f7d1f1f3362c5a9f9d6b0b2f579079
jae1u <yjw_0412@naver.com>
2025-04-28 12:04:58 +0000

    feat: Implement ForwardingUnit and HazardDetectionUnit for forwarding

--- Lab4/HazardDetectionUnit.v
+++ Lab4/HazardDetectionUnit.v
@@ -2,19 +2,15 @@ module HazardDetectionUnit(
     input [4:0] rs1_id,
     input [4:0] rs2_id,
     input [4:0] rd_ex,
-    input [4:0] rd_mem,
-    input reg_write_ex,
-    input reg_write_mem,
+    input mem_read_ex,
     output reg is_stall);
 
+    // TODO: Maybe we neet to check if instruction is really use rs1 or rs2
     wire use_rs1 = (rs1_id != 0);
     wire use_rs2 = (rs2_id != 0);
 
     always @(*) begin
-        is_stall = rs1_id == rd_ex && use_rs1 && reg_write_ex ||
-                   rs2_id == rd_ex && use_rs2 && reg_write_ex ||
-                   rs1_id == rd_mem && use_rs1 && reg_write_mem ||
-                   rs2_id == rd_mem && use_rs2 && reg_write_mem;
+        is_stall = ((rs1_id == rd_ex) && use_rs1 || (rs2_id == rd_ex) && use_rs2) && mem_read_ex;
     end
 
 endmodule
