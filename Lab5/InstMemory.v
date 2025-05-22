8449d0d83ff5af670ff6f08648310dbe3df1322a
jae1u <yjw_0412@naver.com>
2025-05-22 10:38:23 +0000

    refactor: Add commented filename options for easier test case switching

--- Lab5/InstMemory.v
+++ Lab5/InstMemory.v
@@ -21,8 +21,14 @@ module InstMemory #(parameter MEM_DEPTH = 16384) (input reset,
         mem[i] = 32'b0;
         /* verilator lint_on BLKSEQ */
         // DO NOT TOUCH COMMENT ABOVE
-      // Provide path of the file including instructions with binary format
-      $readmemh("/path/to/binary_format/file", mem);
+        // Provide path of the file including instructions with binary format
+        // $readmemh("./student_tb/basic_mem.txt", mem);
+        // $readmemh("./student_tb/ifelse_mem.txt", mem);
+        // $readmemh("./student_tb/loop_mem.txt", mem);
+        // $readmemh("./student_tb/non-controlflow_mem.txt", mem);
+        $readmemh("./student_tb/recursive_mem.txt", mem);
+        // $readmemh("./Lab5_student_tb/naive_matmul_unroll.mem", mem);
+        // $readmemh("./Lab5_student_tb/opt_matmul_unroll.mem", mem);
     end
   end
 
